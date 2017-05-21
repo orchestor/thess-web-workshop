04d4daf3c08bf1ef4bcb5617dca783d9246215da
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-21 18:41:50 +0300

    fixed ready

--- log/log.js
+++ log/log.js
@@ -3,6 +3,6 @@ var hyperlog = require('hyperlog')
 var db = level('log.db')
 var log = hyperlog(db, { valueEncoding: 'json' })
 var timedb = level('time.db')
-require('./timeview.js')({ db: timedb, log: log })
 module.exports = log
+log.dex = require('./timeview.js')({ db: timedb, log: log })
 log.timedb = timedb

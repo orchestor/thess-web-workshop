395d70e7558319c7cb46df8475bb8d26bb0fd9ed
substack <substack@9nTgtYmvW4HID6ayt6Icwc8WZxdifx5SlSKKIX/X/1g=.ed25519>
2017-05-21 13:33:30 +0300

    uc example

---
+++ streams/uc.js
@@ -0,0 +1,8 @@
+var through = require('through2')
+process.stdin
+  .pipe(through(write))
+  .pipe(process.stdout)
+
+function write (buf, enc, next) {
+  next(null, buf.toString().toUpperCase())
+}
